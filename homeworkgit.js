7f8ec46bd82fce961885e27d66b2acfa358ef688
floom412 <vladfloom52747@gmail.com>
2025-08-06 13:41:02 +0300

    convert

--- homeworkgit.js
+++ homeworkgit.js
@@ -1,3 +1,22 @@
 console.log("мій сайт");
 var a = 5;
 console.log(a * 9);
+function smartConvert(uahAmount) {
+    var curseUsd = 41.5;
+    var curseEur = 48.7;
+    var cursePln = 12.1;
+    var usdAmount = uahAmount / curseUsd;
+    var eurAmount = uahAmount / curseEur;
+    var plnAmount = uahAmount / cursePln;
+    var maxAmount = Math.max(usdAmount, eurAmount, plnAmount);
+    if (maxAmount === usdAmount) {
+        console.log("\u043D\u0430\u0439\u0432\u0438\u0433\u0456\u0434\u043D\u0456\u0448\u0430 \u0432\u0430\u043B\u044E\u0442\u0430 \u0434\u043B\u044F \u043F\u043E\u043A\u0443\u043F\u043A\u0438-\u0434\u043E\u043B\u043B\u0430\u0440\u0438. \u0412\u0438 \u043E\u0442\u0440\u0438\u043C\u0443\u0454\u0442\u0435 ".concat(usdAmount, " \u0434\u043E\u043B\u043B\u0430\u0440\u0456\u0432"));
+    }
+    else if (maxAmount === eurAmount) {
+        console.log("\u043D\u0430\u0439\u0432\u0438\u0433\u0456\u0434\u043D\u0456\u0448\u0430 \u0432\u0430\u043B\u044E\u0442\u0430 \u0434\u043B\u044F \u043F\u043E\u043A\u0443\u043F\u043A\u0438-\u0454\u0432\u0440\u043E. \u0412\u0438 \u043E\u0442\u0440\u0438\u043C\u0443\u0454\u0442\u0435 ".concat(eurAmount, " \u0454\u0432\u0440\u043E"));
+    }
+    else {
+        console.log("\u043D\u0430\u0439\u0432\u0438\u0433\u0456\u0434\u043D\u0456\u0448\u0430 \u0432\u0430\u043B\u044E\u0442\u0430 \u0434\u043B\u044F \u043F\u043E\u043A\u0443\u043F\u043A\u0438-\u0437\u043B\u043E\u0442\u0438\u0445. \u0412\u0438 \u043E\u0442\u0440\u0438\u043C\u0443\u0454\u0442\u0435 ".concat(plnAmount.toFixed(2), " \u0437\u043B\u043E\u0442\u0438\u0445"));
+    }
+}
+smartConvert(5000);
--- homeworkgit.ts
+++ homeworkgit.ts
@@ -1,3 +1,34 @@
 console.log("мій сайт");
 let a:number=5;
 console.log(a*9);
+
+function smartConvert(uahAmount:number):void
+{
+    let curseUsd=41.5;
+    let curseEur=48.7;
+    let cursePln=12.1;
+    let usdAmount:number=uahAmount/curseUsd;
+    let eurAmount:number=uahAmount/curseEur;
+    let plnAmount:number=uahAmount/cursePln;
+    let maxAmount:number=Math.max(usdAmount,eurAmount,plnAmount);
+    if (maxAmount===usdAmount)
+    {
+        console.log(`найвигідніша валюта для покупки-доллари. Ви отримуєте ${usdAmount} долларів`);
+        
+    }
+    else if (maxAmount===eurAmount)
+    {
+        console.log(`найвигідніша валюта для покупки-євро. Ви отримуєте ${eurAmount} євро`);
+        
+        
+    }
+    else {
+        console.log(`найвигідніша валюта для покупки-злотих. Ви отримуєте ${plnAmount.toFixed(2)} злотих`);
+        
+        
+    }
+
+}
+
+smartConvert(5000);
+
